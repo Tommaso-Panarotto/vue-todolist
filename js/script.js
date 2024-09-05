8911f0c7baf0c90123664ce558cf3159c293edce
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-09-05 15:20:15 +0200

    inject task in page

--- js/script.js
+++ js/script.js
@@ -4,7 +4,40 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            toDoList: [
+                {
+                    text: 'fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'portare il cane a spasso',
+                    done: false
+                },
+                {
+                    text: 'pulire casa',
+                    done: false
+                },
+                {
+                    text: 'fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'fare esercizio fisico',
+                    done: false
+                },
+                {
+                    text: 'uscire a cena',
+                    done: false
+                },
+                {
+                    text: 'cucinare il pasto',
+                    done: false
+                },
+                {
+                    text: 'gonfiare le gomme',
+                    done: false
+                }
+            ]
         }
     }
 }).mount('#app')
